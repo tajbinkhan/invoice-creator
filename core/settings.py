812f35f0b14ccaecb363074f468c455e590b3b7a
Tajbin Khan <tajbink@gmail.com>
2023-07-14 14:45:54 +0600

    version 1.0

--- core/settings.py
+++ core/settings.py
@@ -110,10 +110,24 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+# 	'default': {
+# 		'ENGINE': 'django.db.backends.sqlite3',
+# 		'NAME': BASE_DIR / 'db.sqlite3',
+# 	}
+# }
+
 DATABASES = {
 	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': BASE_DIR / 'db.sqlite3',
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'invoice-creator',
+		'USER': 'root',
+		'PASSWORD': '',
+		'HOST': '127.0.0.1',
+		'PORT': '3306',
+		'OPTIONS': {
+			'sql_mode': 'STRICT_TRANS_TABLES',
+		},
 	}
 }
 
