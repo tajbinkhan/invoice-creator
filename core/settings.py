4eb8d6fb6dfa4b9cc72aa1b0c290641ab926f4ac
Tajbin Khan <tajbink@gmail.com>
2023-07-14 07:02:59 +0600

    version 1.0

--- core/settings.py
+++ core/settings.py
@@ -33,7 +33,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+if DEBUG:
+	ALLOWED_HOSTS = ['tajmoho.pythonanywhere.com']
+else:
+	ALLOWED_HOSTS = []
 
 
 # Application definition
