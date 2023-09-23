64d894e6a6fba40e5bde47c93dea5b4e1042b62b
Tajbin Khan <tajbink@gmail.com>
2023-09-23 21:13:55 +0600

    Bug Fixed

--- Clients/views.py
+++ Clients/views.py
@@ -8,15 +8,14 @@ from .forms import ClientForm
 from core.decorators import superuser_required
 
 # Create your views here.
-#------------- Client Section Start -------------#
+# ------------- Client Section Start -------------#
 @superuser_required
 def clients(request):
-	template_name = 'clients/index.html'
+	template_name = "clients/index.html"
 	clients = Clients.objects.all()
-	context = {
-		'clients': clients
-	}
+	context = {"clients": clients}
 	return render(request, template_name, context)
+
 
 @superuser_required
 def clients_api(request):
@@ -24,78 +23,87 @@ def clients_api(request):
 
 	data = []
 	for client in clients:
-		locale.setlocale(locale.LC_TIME, 'en_US.UTF-8')
-		updated_at = client.updated_at.strftime('%b %d, %Y')
-		created_at = client.created_at.strftime('%b %d, %Y')
+		locale.setlocale(locale.LC_TIME, "en_US.UTF-8")
+		updated_at = client.updated_at.strftime("%b %d, %Y")
+		created_at = client.created_at.strftime("%b %d, %Y")
 
-		data.append({
-			'client_id': client.client_id,
-			'client_name': client.client_name,
-			'office_address': truncatewords(client.office_address, 3),
-			'updated_at': updated_at,
-			'created_at': created_at,
-		})
+		data.append(
+			{
+				"client_id": client.client_id,
+				"client_name": client.client_name,
+				"office_address": truncatewords(client.office_address, 3),
+				"updated_at": updated_at,
+				"created_at": created_at,
+			}
+		)
 
 	response = {
-		'data': data,
+		"data": data,
 	}
 	return JsonResponse(response)
 
+
 @superuser_required
 def client_name_api(request):
-	search_term = request.GET.get('q', '')
-	clients = Clients.objects.filter(client_name__icontains=search_term).values('id', 'client_name')
+	search_term = request.GET.get("q", "")
+	clients = Clients.objects.filter(client_name__icontains=search_term).values(
+		"id", "client_name"
+	)
 	return JsonResponse(list(clients), safe=False)
+
 
 @superuser_required
 def add_client(request):
-	template_name = 'clients/form.html'
-	if request.method == 'POST':
+	template_name = "clients/form.html"
+	if request.method == "POST":
 		form = ClientForm(request.POST)
 		if form.is_valid():
 			form.save()
-			messages.success(request, 'Client added successfully!')
-			return redirect('clients')
+			messages.success(request, "Client added successfully!")
+			return redirect("clients")
 		else:
-			messages.error(request, 'Client added failed!')
+			messages.error(request, "Client added failed!")
 	else:
 		form = ClientForm()
 	context = {
-		'form': form,
-		'head_title': 'Add New Client',
-		'button_name': 'Add Client'
+		"form": form,
+		"head_title": "Add New Client",
+		"button_name": "Add Client",
 	}
 	return render(request, template_name, context)
+
 
 @superuser_required
 def update_client(request, client_id):
-	template_name = 'clients/form.html'
+	template_name = "clients/form.html"
 	client = get_object_or_404(Clients, client_id=client_id)
 	form = ClientForm(request.POST or None, instance=client)
-	if request.method == 'POST':
+	if request.method == "POST":
 		form = ClientForm(request.POST, instance=client)
 		if form.is_valid():
 			form.save()
-			messages.success(request, f'{client.client_name} updated successfully!')
-			return redirect('clients')
+			messages.success(request, f"{client.client_name} updated successfully!")
+			return redirect("clients")
 		else:
-			messages.error(request, 'Client updated failed!')
+			messages.error(request, "Client updated failed!")
 	context = {
-		'form': form,
-		'head_title': 'Update Client',
-		'button_name': 'Update Client'
+		"form": form,
+		"head_title": "Update Client",
+		"button_name": "Update Client",
 	}
 	return render(request, template_name, context)
 
+
 @superuser_required
 def delete_client(request, client_id):
-	if request.method == 'POST':
+	if request.method == "POST":
 		client = get_object_or_404(Clients, client_id=client_id)
 		client.delete()
-		messages.success(request, f'{client.client_name} deleted successfully!')
-		return redirect('clients')
+		messages.success(request, f"{client.client_name} deleted successfully!")
+		return redirect("clients")
 	else:
-		messages.error(request, 'Client deleted failed!')
-		return redirect('clients')
+		messages.error(request, "Client deleted failed!")
+		return redirect("clients")
 
-#------------- Client Section End -------------#
+
+# ------------- Client Section End -------------#
--- ProformaInvoice/views.py
+++ ProformaInvoice/views.py
@@ -13,50 +13,75 @@ from core.decorators import superuser_required
 # Create your views here.
 @superuser_required
 def index(request):
-	template_name = 'proforma/index.html'
+	template_name = "proforma/index.html"
 	return render(request, template_name)
+
 
 @superuser_required
 def list_proforma_invoice_api(request):
-	invoice_list = ProformaInvoice.objects.all().prefetch_related(Prefetch('proforma_goods', queryset=ProformaInvoiceGoods.objects.all().order_by('id')))
+	invoice_list = ProformaInvoice.objects.all().prefetch_related(
+		Prefetch(
+			"proforma_goods", queryset=ProformaInvoiceGoods.objects.all().order_by("id")
+		)
+	)
 
 	invoice_data = []
 
 	for invoice in invoice_list:
-		locale.setlocale(locale.LC_TIME, 'en_US.UTF-8')
-		date = invoice.created_at.strftime('%b %d, %Y')
-		created_at = invoice.created_at.strftime('%b %d, %Y')
+		locale.setlocale(locale.LC_TIME, "en_US.UTF-8")
+		date = invoice.created_at.strftime("%b %d, %Y")
+		created_at = invoice.created_at.strftime("%b %d, %Y")
 
 		goods_data = []
 		for goods in invoice.proforma_goods.all():
-			goods_data.append({
-				'product': goods.product,
-				'quantity': goods.quantity,
-				'unit': goods.unit,
-				'unit_price': goods.unit_price
-			})
+			goods_data.append(
+				{
+					"product": goods.product,
+					"quantity": goods.quantity,
+					"unit": goods.unit,
+					"unit_price": goods.unit_price,
+				}
+			)
 
-		invoice_data.append({
-			'invoice_id': invoice.invoice_id,
-			'invoice_number': invoice.invoice_number,
-			'client': invoice.client.client_name,
-			'goods_data': goods_data,
-			'currency': invoice.currency,
-			'custom_total_amount': invoice.custom_total_amount,
-			'calculate_total_goods_value': invoice.calculate_total_goods_value(),
-			'date': date,
-			'created_at': created_at
-		})
+		try:
+			invoice_data.append(
+				{
+					"invoice_id": invoice.invoice_id,
+					"invoice_number": invoice.invoice_number,
+					"client": invoice.client.client_name,
+					"goods_data": goods_data,
+					"currency": invoice.currency,
+					"custom_total_amount": invoice.custom_total_amount,
+					"calculate_total_goods_value": invoice.calculate_total_goods_value(),
+					"date": date,
+					"created_at": created_at,
+				}
+			)
+		except:
+			invoice_data.append(
+				{
+					"invoice_id": invoice.invoice_id,
+					"invoice_number": invoice.invoice_number,
+					"client": "No Client",
+					"goods_data": goods_data,
+					"currency": invoice.currency,
+					"custom_total_amount": invoice.custom_total_amount,
+					"calculate_total_goods_value": invoice.calculate_total_goods_value(),
+					"date": date,
+					"created_at": created_at,
+				}
+			)
 
 	response = {
-		'invoice_data': invoice_data,
+		"invoice_data": invoice_data,
 	}
 	return JsonResponse(response)
 
+
 @superuser_required
 def add_invoice(request):
-	template_name = 'proforma/form.html'
-	if request.method == 'POST':
+	template_name = "proforma/form.html"
+	if request.method == "POST":
 		form = ProformaInvoiceForm(request.POST)
 		formset = ProformaInvoiceGoodsFormSet(request.POST)
 		if form.is_valid() and formset.is_valid():
@@ -64,85 +89,116 @@ def add_invoice(request):
 			formset.instance = invoice
 			formset.save()
 			messages.success(request, "Invoice created successfully!")
-			return redirect('proforma_invoice')
+			return redirect("proforma_invoice")
 	else:
 		form = ProformaInvoiceForm()
 		formset = ProformaInvoiceGoodsFormSet()
 	context = {
-		'form': form,
-		'formset': formset,
-		'head_title': 'Proforma Invoice Form',
-		'button_name': 'Add Invoice'
+		"form": form,
+		"formset": formset,
+		"head_title": "Proforma Invoice Form",
+		"button_name": "Add Invoice",
 	}
 	return render(request, template_name, context)
 
+
 @superuser_required
 def detail_invoice(request, invoice_id):
-	template_name = 'proforma/details.html'
+	template_name = "proforma/details.html"
 	preferences = Preferences.objects.last()
-	proforma_invoice = ProformaInvoice.objects.prefetch_related('proforma_goods').get(invoice_id=invoice_id)
+	proforma_invoice = ProformaInvoice.objects.prefetch_related("proforma_goods").get(
+		invoice_id=invoice_id
+	)
 	description = Description.objects.last()
 
 	number2words = None
 	if proforma_invoice.custom_total_amount:
 		number_str = str(proforma_invoice.custom_total_amount)
-		number = float(number_str.replace(',', ''))
-		integer_words = num2words(math.floor(number), lang='en_IN').replace(',', '').replace(' and ', ' ').replace('lakh', 'lac')
-		integer_part, decimal_part = number_str.split('.')
-		decimal_words = num2words(decimal_part, lang='en_IN').replace(',', '').replace(' and ', ' ').replace('lakh', 'lac')
+		number = float(number_str.replace(",", ""))
+		integer_words = (
+			num2words(math.floor(number), lang="en_IN")
+			.replace(",", "")
+			.replace(" and ", " ")
+			.replace("lakh", "lac")
+		)
+		integer_part, decimal_part = number_str.split(".")
+		decimal_words = (
+			num2words(decimal_part, lang="en_IN")
+			.replace(",", "")
+			.replace(" and ", " ")
+			.replace("lakh", "lac")
+		)
 		if int(decimal_part) > 0:
 			number2words = f"{integer_words} Point {decimal_words}"
 		else:
 			number2words = integer_words
 	else:
 		number_str = str(proforma_invoice.calculate_total_goods_value())
-		number = float(number_str.replace(',', ''))
-		integer_words = num2words(math.floor(number), lang='en_IN').replace(',', '').replace(' and ', ' ').replace('lakh', 'lac')
+		number = float(number_str.replace(",", ""))
+		integer_words = (
+			num2words(math.floor(number), lang="en_IN")
+			.replace(",", "")
+			.replace(" and ", " ")
+			.replace("lakh", "lac")
+		)
 		decimal_part = round((number % 1) * 100)
-		decimal_words = num2words(decimal_part, lang='en_IN').replace(',', '').replace(' and ', ' ').replace('lakh', 'lac')
+		decimal_words = (
+			num2words(decimal_part, lang="en_IN")
+			.replace(",", "")
+			.replace(" and ", " ")
+			.replace("lakh", "lac")
+		)
 		if decimal_part > 0:
 			number2words = f"{integer_words} Point {decimal_words}"
 		else:
 			number2words = integer_words
 
 	context = {
-		'proforma_invoice': proforma_invoice,
-		'preferences': preferences,
-		'number2words': number2words,
-		'description': description
+		"proforma_invoice": proforma_invoice,
+		"preferences": preferences,
+		"number2words": number2words,
+		"description": description,
 	}
 	return render(request, template_name, context)
 
+
 @superuser_required
 def update_invoice(request, invoice_id):
-	template_name = 'proforma/form.html'
+	template_name = "proforma/form.html"
 	proforma_invoice_id = ProformaInvoice.objects.get(invoice_id=invoice_id)
-	if request.method == 'POST':
+	if request.method == "POST":
 		form = ProformaInvoiceForm(request.POST, instance=proforma_invoice_id)
-		formset = ProformaInvoiceGoodsFormSet(request.POST, instance=proforma_invoice_id)
+		formset = ProformaInvoiceGoodsFormSet(
+			request.POST, instance=proforma_invoice_id
+		)
 		if form.is_valid() and formset.is_valid():
 			invoice = form.save()
 			formset.instance = invoice
 			formset.save()
-			messages.success(request, f'{proforma_invoice_id.invoice_number} updated successfully!')
-			return redirect('proforma_invoice')
+			messages.success(
+				request, f"{proforma_invoice_id.invoice_number} updated successfully!"
+			)
+			return redirect("proforma_invoice")
 	else:
 		form = ProformaInvoiceForm(instance=proforma_invoice_id)
 		formset = ProformaInvoiceGoodsFormSet(instance=proforma_invoice_id)
 	context = {
-		'form': form,
-		'formset': formset,
-		'head_title': 'Proforma Invoice Form',
-		'button_name': 'Update Invoice'
+		"form": form,
+		"formset": formset,
+		"head_title": "Proforma Invoice Form",
+		"button_name": "Update Invoice",
 	}
 	return render(request, template_name, context)
+
 
 @superuser_required
 def delete_invoice(request, invoice_id):
 	proforma_invoice_id = ProformaInvoice.objects.get(invoice_id=invoice_id)
 	if request.method == "POST":
 		proforma_invoice_id.delete()
-		messages.success(request, f'{proforma_invoice_id.invoice_number} deleted successfully!')
-		return redirect('proforma_invoice')
+		messages.success(
+			request, f"{proforma_invoice_id.invoice_number} deleted successfully!"
+		)
+		return redirect("proforma_invoice")
 
-	return render(request, 'base/delete_confirmation.html')
+	return render(request, "base/delete_confirmation.html")
